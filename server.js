950b7c47edc731512046ec132142af0975e6a244
Jeongho Han <jungho1109@gmail.com>
2020-06-22 03:22:09 -0700

    fix error

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.json({ extended: false }));
 
 app.use("/users", require("./routes/users"));
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server started on port ${process.env.PORT || 5000}`)
-);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+app.listen(process.env.PORT || 5000, () => console.log(`Server is On`));
