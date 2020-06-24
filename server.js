69e7c537250831279ab563cc082df432ef79115f
Jeongho Han <jungho1109@gmail.com>
2020-06-23 21:04:43 -0700

    Cut off Mongo

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 const express = require("express");
-const connectDB = require("./config/db");
 
 const app = express();
 
-connectDB();
-
 app.use(express.json({ extended: false }));
-
-app.use("/users", require("./routes/users"));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
 
 app.listen(process.env.PORT || 5000, () => console.log(`Server is On`));
