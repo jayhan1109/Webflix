1905257d0276bf84f7dfc949eaa42da0c80120fc
Jeongho Han <jungho1109@gmail.com>
2020-06-23 23:42:07 -0700

    Login Function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import "./firebaseui-styling.global.css";
 import Entry from "./components/layout/entry/Entry";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Auth from "./components/layout/Auth/Auth";
 import Landing from "./components/layout/Landing/Landing";
 
@@ -10,14 +10,32 @@ import Landing from "./components/layout/Landing/Landing";
 import firebase from "./service/firebase";
 
 const App = () => {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    firebaseAuth();
+  }, []);
+
+  const firebaseAuth = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setAuth(true);
+      } else {
+        setAuth(false);
+      }
+    });
+  };
+
+  if (auth) {
+    return <Redirect exact to="/landing" />;
+  }
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Entry} />
-        <Route exact path="/auth" component={Auth} />
-        <Route exact path="/landing" component={Landing} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Entry} />
+      <Route exact path="/auth" component={Auth} />
+      <Route exact path="/landing" component={Landing} />
+    </Switch>
   );
 };
 
